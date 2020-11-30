cc94bc3d238fc5441f14658634715b71af769edd
bsteve456 <stevealan1@protonmail.com>
2020-11-30 20:46:45 +0100

    finish first exo

--- Module-00/ex00/megaphone.cpp
+++ Module-00/ex00/megaphone.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int main()
+int main(int ac, char **av)
 {
-	std::cout << "Hello World\n";
+	std::locale loc;
+	std::string str;
+	if (ac > 1)
+	{	
+		for (int i = 1; i < ac; i++)
+		{
+			str = av[i];
+			for (std::string::size_type i = 0; i < str.length(); i++)
+				std::cout << std::toupper(str[i], loc);
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << "\n";
 }
