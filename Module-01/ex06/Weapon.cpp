a81c53d7732ea1a12febe90a8ef6d5c8cd004af7
bsteve456 <stevealan1@protonmail.com>
2020-12-03 11:23:02 +0100

    add weapon cpp and hpp

---
+++ Module-01/ex06/Weapon.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 11:12:12 by stbaleba          #+#    #+#             */
+/*   Updated: 2020/12/03 11:21:40 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type1)
+{
+	type = type1;
+}
+
+const std::string	Weapon::getType()
+{
+	return ((const)type);
+}
+
+void Weapon::setType(std::string type)
+{
+	type = type1;
+}
---
+++ Module-01/ex06/Weapon.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 11:12:08 by stbaleba          #+#    #+#             */
+/*   Updated: 2020/12/03 11:18:09 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <iostream>
+
+class Weapon
+{
+	private:
+		std::string	type;
+	public:
+		Weapon(std::string type1);
+		const	std::string getType();
+		void	setType(std::string type1);
+}
+
+#endif
