a5773fed140989b24a51ffbf384dd81098b72d90
bsteve456 <stevealan1@protonmail.com>
2020-12-10 00:06:06 +0100

    add assault Terminator

---
+++ Module-04/ex02/AssaultTerminator.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AssaultTerminator.cpp                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 23:42:04 by stbaleba          #+#    #+#             */
+/*   Updated: 2020/12/10 00:05:49 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AssaultTerminator.hpp"
+
+void AssaultTerminator::battleCry()
+{
+	std::cout << "This code is unclean PURIFY IT!\n";
+}
+
+void AssaultTerminator::rangedAttack()
+{
+	std::cout << "* does nothing *\n";
+}
+
+void AssaultTerminator::meleeAttack()
+{
+	std::cout << "* attacks with chainfists *\n";
+}
+
+ISpaceMarine * AssaultTerminator::clone()
+{
+	AssaultTerminator *copy = new AssaultTerminator;
+	return (copy);
+}
---
+++ Module-04/ex02/AssaultTerminator.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AssaultTerminator.hpp                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 23:42:08 by stbaleba          #+#    #+#             */
+/*   Updated: 2020/12/10 00:04:05 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ASSAULT_TERMINATOR_HPP
+# define ASSAULT_TERMINATOR_HPP
+
+# include "ISpaceMarine.hpp"
+
+class 	AssaultTerminator : public ISpaceMarine
+{
+	public:
+		AssaultTerminator() : ISpaceMarine()
+		{
+			std::cout << "* teleports from space *\n";
+		}
+		~AssaultTerminator()
+		{
+			std::cout << "I'll be back...\n";
+		}
+		ISpaceMarine *clone();
+		void	battleCry() const;
+		void	rangedAttack() const;
+		void	meleeAttack() const;
+
+
+};
+
+#endif
