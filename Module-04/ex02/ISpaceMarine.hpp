727dae4de5f7dfb0277afaf92dbaab82d6dec339
bsteve456 <stevealan1@protonmail.com>
2020-12-09 23:40:24 +0100

    add ISpaceMarine.hpp

---
+++ Module-04/ex02/ISpaceMarine.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ISpaceMarine.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 23:36:02 by stbaleba          #+#    #+#             */
+/*   Updated: 2020/12/09 23:39:54 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ISPACE_MARINE_HPP
+# define ISPACE_MARINE_HPP
+
+class ISpaceMarine()
+{
+	public:
+		virtual ~ISpaceMarine() {}
+		virtual ISpaceMarine* clone() const = 0;
+		virtual void battleCry() const = 0;
+		virtual void rangedAttack() const = 0;
+		virtual void meleeAttack() const = 0;
+};
+
+#endif
