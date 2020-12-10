8361167dc6d9811b2d1c83493a05b971296cd363
bsteve456 <stevealan1@protonmail.com>
2020-12-10 13:11:02 +0100

    add IMateriaSource

---
+++ Module-04/ex03/IMateriaSource.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/10 13:07:39 by stbaleba          #+#    #+#             */
+/*   Updated: 2020/12/10 13:10:47 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IMATERIA_SOURCE_HPP
+# define IMATERIA_SOURCE_HPP
+
+# include "iostream"
+
+class IMateriaSource
+{
+	public:
+		virtual ~IMateriaSource() {}
+		virtual void learnMateria(AMateria*) = 0;
+		virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif
