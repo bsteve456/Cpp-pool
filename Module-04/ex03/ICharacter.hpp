9f70705a48c0976fcfa8ac8df1a1e4522450dc61
bsteve456 <stevealan1@protonmail.com>
2020-12-10 12:06:53 +0100

    add ICharacter.hpp

---
+++ Module-04/ex03/ICharacter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/10 12:02:44 by stbaleba          #+#    #+#             */
+/*   Updated: 2020/12/10 12:06:31 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICHARACTER_HPP
+# define ICHARACTER_HPP
+
+# include <iostream>
+
+class ICharacter
+{
+	public:
+		virtual ~ICharacter() {}
+		virtual std::string const & getName() const = 0;
+		virtual void equip(AMateria* m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter& target) = 0;
+};
+
+#endif
