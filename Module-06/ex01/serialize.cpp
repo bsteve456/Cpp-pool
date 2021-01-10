3e1bcd7421f3833cd9d54588a1e9d288b47a6bbe
bsteve456 <stevealan1@protonmail.com>
2021-01-10 13:28:58 +0100

    add som test

--- Module-06/ex01/serialize.cpp
+++ Module-06/ex01/serialize.cpp
@@ -6,7 +6,7 @@
 /*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 13:53:54 by stbaleba          #+#    #+#             */
-/*   Updated: 2021/01/09 22:19:56 by stbaleba         ###   ########.fr       */
+/*   Updated: 2021/01/10 13:27:50 by stbaleba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,8 @@ int main()
 	std::cout << "s1 : "<< res->s1 << std::endl;
 	std::cout << "n : " << res->n << std::endl;
 	std::cout << "s2 : " << res->s2 << std::endl;
+
+/*	std::cout << "Data size : "<< sizeof(res->s1) + sizeof(res->n) + sizeof(res->s2) << std::endl;
+	std::cout << "ask size : " << sizeof(std::string) + sizeof(int) + sizeof(std::string) << std::endl;*/
 	delete(res);
 }
