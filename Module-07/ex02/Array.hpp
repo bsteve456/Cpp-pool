1c4a001b0ff27abec69d30b6dc47f787289a7999
bsteve456 <stevealan1@protonmail.com>
2021-01-12 16:25:52 +0100

    finish ex02

---
+++ Module-07/ex02/Array.hpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 13:01:27 by stbaleba          #+#    #+#             */
+/*   Updated: 2021/01/12 16:14:07 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <exception>
+
+template < typename T >
+class Array
+{
+	public:
+		class OutOfLimits : std::exception
+		{
+			public:
+				const char * what() const throw() {
+					return "Out of Limits! ";
+				}
+		};
+		Array()
+		{
+			this->arr = 0;
+				this->length = 0;
+		}
+		Array(unsigned int n)
+		{
+			this->length = n;
+			this->arr = new T[n];
+			for (unsigned int i = 0; i < n; i++)
+				this->arr[i] = 0;
+		}
+		Array<T>(Array<T> const &t)
+		{
+			*this = t;
+		}
+		Array<T> & operator = (Array<T> const &t)
+		{
+			if (this != &t )
+			{
+				this->length = t.size();
+				this->arr = new T[this->length];
+				for (unsigned int i = 0; i < t.size(); i++)
+					this->arr[i] = t[i];
+			}
+			return (*this);
+		}
+		T operator [] (unsigned int n) const
+		{
+			try
+			{
+				if (n >= this->length)
+					throw OutOfLimits();
+				return (this->arr[n]);
+			}
+			catch (OutOfLimits &e)
+			{
+				std::cerr << "Error : " << e.what();
+				return (static_cast<T>(0));
+			}
+		}
+		void	setArray(unsigned int elem, T val)
+		{
+			if (elem < this->length)
+				this->arr[elem] = val;
+		}
+		unsigned int size() const
+		{
+			return (this->length);
+		}
+		~Array() {
+			if(this->arr != 0)
+				delete [] this->arr;
+		}
+	private:
+		T *arr;
+		unsigned int length;
+};
+#endif
---
+++ Module-07/ex02/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 13:13:26 by stbaleba          #+#    #+#             */
+/*   Updated: 2021/01/12 16:25:25 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Array.hpp"
+
+int main()
+{
+	std::cout << "INT ARRAY" << std::endl;
+	Array<int> n;
+	std::cout << "size : " << n.size() << " " << n[2] << std::endl;
+	Array<int> n1((unsigned int)5);
+	std::cout << "size : " << n1.size() << " " << n1[2] << std::endl;
+	Array<int> n2(n1);
+	std::cout << "size : " << n2.size() << " " << n2[2] << std::endl;
+	n2.setArray(2, 456);
+	n2.setArray(3, -545);
+	n2.setArray(4, 45);
+	for (unsigned int i = 0; i < n2.size(); i++)
+		std::cout << n2[i] << " ";
+	std::cout << std::endl;
+	Array<int>n3;
+	n3 = n2;
+	std::cout << "TEST COPY" <<std::endl;
+	for (unsigned int i = 0; i < n3.size(); i++)
+		std::cout << n3[i] << " ";
+	std::cout << std::endl;
+	std::cout << "CHAR ARRAY" << std::endl;
+	Array<char> c;
+	std::cout << "size : " << c.size() << " " << c[2] << std::endl;
+	Array<char> c1((unsigned int)5);
+	std::cout << "size : " << c1.size() << " " << c1[2] << std::endl;
+	Array<char> c2(c1);
+	std::cout << "size : " << c2.size() << " " << c2[2] << std::endl;
+	c2.setArray(0, 'H');
+	c2.setArray(1, 'E');
+	c2.setArray(2, 'L');
+	c2.setArray(3, 'L');
+	c2.setArray(4, 'O');
+	for (unsigned int i = 0; i < c2.size(); i++)
+		std::cout << c2[i] << " ";
+	std::cout << std::endl;
+	Array<char>c3;
+	c3 = c2;
+	std::cout << "TEST COPY" <<std::endl;
+	for (unsigned int i = 0; i < c3.size(); i++)
+		std::cout << c3[i] << " ";
+	std::cout << std::endl;
+}
