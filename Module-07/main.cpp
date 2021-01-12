c172839990e21a948846b8fa3de6f1e2f2a0f928
bsteve456 <stevealan1@protonmail.com>
2021-01-12 11:44:16 +0100

    finish ex00

---
+++ Module-07/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 11:29:35 by stbaleba          #+#    #+#             */
+/*   Updated: 2021/01/12 11:42:25 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
---
+++ Module-07/whatever.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stbaleba <stbaleba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 11:33:12 by stbaleba          #+#    #+#             */
+/*   Updated: 2021/01/12 11:42:48 by stbaleba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template < typename T >
+void swap(T &a, T &b)
+{
+	T temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template < typename T >
+T const & min (T const & x, T const & y )
+{
+	return ((x <= y) ? x : y);
+}
+
+template < typename T >
+T const & max (T const & x, T const & y )
+{
+	return ((x >= y) ? x : y);
+}
+#endif
